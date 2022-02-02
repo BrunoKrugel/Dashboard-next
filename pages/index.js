abc80efdafbdba00e32950a7e0b471687d54decf
Bruno Krugel <brunokrugel@gmail.com>
2022-02-01 23:42:34 -0300

    mongoDB

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 import {Button, TextField, Paper} from '@mui/material';
+import clientPromise from '../lib/mongodb'
 
-export default function Home() {
+export default function Home({ isConnected }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,6 +17,14 @@ export default function Home() {
       <main className={styles.main}>
 
         <Paper className={styles.card} elevation={3}>
+        {isConnected ? (
+          <h2 className="subtitle">You are connected to MongoDB</h2>
+        ) : (
+          <h2 className="subtitle">
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions.
+          </h2>
+        )}
         <TextField id="user" label="User" variant="outlined" />
         <TextField id="password" label="Password" variant="outlined" type='password' />
         <div>
@@ -26,4 +35,23 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  try {
+    // client.db() will be the default database passed in the MONGODB_URI
+    // You can change the database by calling the client.db() function and specifying a database like:
+    // const db = client.db("myDatabase");
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+    await clientPromise
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
 }
