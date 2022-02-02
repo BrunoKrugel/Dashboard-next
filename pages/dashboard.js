665087365eaa07292b0ff977662d3cc9d2bb40af
Bruno Krugel <brunokrugel@gmail.com>
2022-02-02 20:03:23 -0300

    Update dashboard.js

---
+++ pages/dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+const dashboard = () => {
+  return (
+    <div>
+      {' '}
+      oi
+      <Link href="/">volta</Link>
+    </div>
+  );
+};
+
+export default dashboard;
