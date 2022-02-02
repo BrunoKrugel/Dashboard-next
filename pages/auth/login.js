cb6da0713a03e519459098d6ba313a0336995dae
Bruno Krugel <brunokrugel@gmail.com>
2022-02-01 21:03:55 -0300

    Initial commit, dashboard

---
+++ pages/auth/login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+const login = () => {
+    return ( <div> oi
+        
+        <Link href="/">
+          volta
+        </Link>
+        
+         </div>
+        
+        );
+}
+ 
+export default login;
