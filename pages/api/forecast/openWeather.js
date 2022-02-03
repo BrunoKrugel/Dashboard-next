491c7280306d365eb6f7d116daf8320bb9cc31c2
Bruno Krugel <brunokrugel@gmail.com>
2022-02-03 17:45:33 -0300

    openWeather

--- pages/api/forecast/openWeather.js
+++ pages/api/forecast/openWeather.js
@@ -1,3 +1,27 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' });
+export default async function handler(req, res) {
+    var axios = require("axios").default;
+    var options = {
+        method: 'GET',
+        url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+        params: {
+            q: 'London,uk',
+            lat: '0',
+            lon: '0',
+            callback: 'test',
+            id: '2172797',
+            lang: 'null',
+            units: 'imperial',
+            mode: 'xml'
+        },
+        headers: {
+            'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+            'x-rapidapi-key': '41aaa4e7c5msh6483e4ef6f7a679p180768jsn6c78bc21973d'
+        }
+    };
+
+    axios.request(options).then(function (response) {
+        res.status(200).json(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
 }
