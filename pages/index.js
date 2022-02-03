b21bf27704f15d57f868f81558655dd0aa934ca3
Bruno Krugel <brunokrugel@gmail.com>
2022-02-02 22:41:15 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,32 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '../styles/Home.module.css';
 import { Button, TextField, Paper, Alert } from '@mui/material';
 import clientPromise from '../lib/mongodb';
 import axios from 'axios';
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  const username = e.target.user.value;
-  const password = e.target.password.value;
-  try {
-    await axios.post(`${window.location.origin}/api/auth/login`, {
-      username,
-      password,
-    });
-    localStorage.setItem('isLogged', 'true');
-    localStorage.setItem('username', username);
-  } catch (error) {
-    localStorage.setItem('isLogged', '');
-  }
-};
-
-const createUser = async (e) => {
-  console.log('oi');
-};
 
 export default function Home({ isConnected }) {
+  const router = useRouter();
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const username = e.target.user.value;
+    const password = e.target.password.value;
+    try {
+      await axios.post(`${window.location.origin}/api/auth/login`, {
+        username,
+        password,
+      });
+      localStorage.setItem('isLogged', 'true');
+      localStorage.setItem('username', username);
+      router.push('/dashboard');
+    } catch (error) {
+      localStorage.setItem('isLogged', '');
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -38,18 +38,18 @@ export default function Home({ isConnected }) {
       <main className={styles.main}>
         <form onSubmit={handleSubmit}>
           <Paper className={styles.card} elevation={3}>
-            {isConnected ? (
-              <Alert severity="success">DB connected!</Alert>
-            ) : (
-              <Alert severity="error">DB not connected!</Alert>
-            )}
             <TextField id="user" label="User" variant="outlined" />
             <TextField
               id="password"
               label="Password"
               variant="outlined"
               type="password"
-            />
+              />
+            {isConnected ? (
+              <Alert severity="success">DB connected!</Alert>
+            ) : (
+              <Alert severity="error">DB not connected!</Alert>
+            )}
             <div>
               <Button type="submit" variant="contained">
                 Login
@@ -67,11 +67,6 @@ export default function Home({ isConnected }) {
 
 export async function getServerSideProps(context) {
   try {
-    // client.db() will be the default database passed in the MONGODB_URI
-    // You can change the database by calling the client.db() function and specifying a database like:
-    // const db = client.db("myDatabase");
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
     await clientPromise;
     return {
       props: { isConnected: true },
