80a36f4f5846e760441df3cb669197efab911428
Bruno Krugel <brunokrugel@gmail.com>
2022-02-03 18:21:08 -0300

    fix rapid api

--- pages/api/forecast/openWeather.js
+++ pages/api/forecast/openWeather.js
@@ -1,21 +1,19 @@
 export default async function handler(req, res) {
+    const uri = process.env.RAPIDAPI_KEY;
     var axios = require("axios").default;
     var options = {
         method: 'GET',
         url: 'https://community-open-weather-map.p.rapidapi.com/weather',
         params: {
-            q: 'London,uk',
-            lat: '0',
-            lon: '0',
+            q: 'Canoas,BR',
             callback: 'test',
-            id: '2172797',
             lang: 'null',
-            units: 'imperial',
+            units: 'metric',
             mode: 'xml'
         },
         headers: {
             'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-            'x-rapidapi-key': '41aaa4e7c5msh6483e4ef6f7a679p180768jsn6c78bc21973d'
+            'x-rapidapi-key': uri
         }
     };
 
