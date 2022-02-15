a3d3f36712528955a5a6fa06caacd5031c026f12
Bruno Krugel <brunokrugel@gmail.com>
2022-02-15 18:15:32 -0300

    resources & forecastMonth

---
+++ pages/api/forecast/forecastMonth.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+
+    var axios = require("axios").default;
+    const uri = process.env.RAPIDAPI_KEY;
+
+    var options = {
+        method: 'GET',
+        url: 'https://community-open-weather-map.p.rapidapi.com/climate/month',
+        params: {
+            q: 'San Francisco'
+        },
+        headers: {
+            'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+            'x-rapidapi-key': uri
+        }
+    };
+
+    axios
+    .request(options)
+    .then(function (response) {
+      res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
