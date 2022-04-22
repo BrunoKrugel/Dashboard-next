1149d1345ab892d8e06f7991264c339fed2acea5
brunokrugel <brunokrugel@gmail.com>
2022-04-22 22:16:44 +0044

    Validation in user creation

--- pages/auth/createUser.js
+++ pages/auth/createUser.js
@@ -19,6 +19,32 @@ function createSeverity(code) {
 }
 
 function validateForm(e) {
+  if (
+    e.target.user.value === '' ||
+    e.target.password.value === '' ||
+    e.target.email.value === ''
+  ) {
+    let field = !e.target.user.value
+      ? e.target.user.name
+      : !e.target.password.value
+      ? e.target.password.name
+      : e.target.email.name;
+    message.text = `Por favor, preencha o campo de ${field}.`;
+    message.severity = 'error';
+    return false;
+  }
+
+  if (e.target.password.value !== e.target.passwordconfirm.value) {
+    message.text = 'Senhas não conferem';
+    message.severity = 'error';
+    return false;
+  }
+
+  if (e.target.email.value.includes('@') === false) {
+    message.text = 'E-mail inválido';
+    message.severity = 'error';
+    return false;
+  }
   return true;
 }
 
@@ -27,23 +53,27 @@ export default function CreateUser() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const username = e.target.user.value;
-    const password = e.target.password.value;
-    const email = e.target.email.value;
-    try {
-      await axios
-        .post(`${window.location.origin}/api/db/user`, {
-          username,
-          password,
-          email,
-        })
-        .then((res) => {
-          message.text = res.data.message;
-          message.severity = createSeverity(res.data.code);
-          setOpen(true);
-        });
-    } catch (error) {
-      console.log(error);
+    if (validateForm(e)) {
+      const username = e.target.user.value;
+      const password = e.target.password.value;
+      const email = e.target.email.value;
+      try {
+        await axios
+          .post(`${window.location.origin}/api/db/user`, {
+            username,
+            password,
+            email,
+          })
+          .then((res) => {
+            message.text = res.data.message;
+            message.severity = createSeverity(res.data.code);
+            setOpen(true);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      setOpen(true);
     }
   };
 
@@ -51,7 +81,8 @@ export default function CreateUser() {
     if (reason === 'clickaway') {
       return;
     }
-
+    message.severity = undefined;
+    message.text = '';
     setOpen(false);
   };
 
@@ -65,14 +96,32 @@ export default function CreateUser() {
       <main className={styles.main}>
         <form onSubmit={handleSubmit}>
           <Paper className={styles.card} elevation={3}>
-            <TextField id="user" label="Usuário" variant="outlined" />
+            <TextField
+              id="user"
+              label="Usuário"
+              variant="outlined"
+              name="Usuário"
+            />
             <TextField
               id="password"
               label="Senha"
               variant="outlined"
               type="password"
+              name="Senha"
             />
-            <TextField id="email" label="E-mail" variant="outlined" />
+            <TextField
+              id="passwordconfirm"
+              label="Confirme a senha"
+              variant="outlined"
+              type="password"
+              name="Confirme a senha"
+            />
+            <TextField
+              id="email"
+              label="E-mail"
+              variant="outlined"
+              name="E-mail"
+            />
             <div>
               <Button type="submit" variant="contained">
                 Registrar
