bbdea236b7acfaf67ff18837de2c38da050bfd43
Bruno Krugel <brunokrugel@gmail.com>
2022-04-29 01:26:14 -0300

    Cypress fix

--- cypress/integration/landing.spec.js
+++ cypress/integration/landing.spec.js
@@ -1,11 +1,11 @@
-describe('Navigation', () => {
-  it('should navigate to the about page', () => {
+describe('Navigation:', () => {
+  it('should navigate to the login page', () => {
     // Start from the index page
-    //cy.visit('http://localhost:3000/');
+    cy.visit('http://localhost:3000/');
     // Find a link with an href attribute containing "about" and click it
-    //cy.get('a[href*="loginn"]').click();
+    cy.get('a[href*="login"]').click();
     // The new url should include "/about"
-    //cy.url().should('include', '/login');
+    cy.url().should('include', '/login');
     // The new page should contain an h1 with "About page"
     //cy.get('h1').contains('About Page');
   });
