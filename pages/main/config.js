7f4960cb15bae5941057b240304307a7195f5878
Bruno Krugel <brunokrugel@gmail.com>
2022-05-18 18:58:23 -0300

    Config

--- pages/main/config.js
+++ pages/main/config.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
-import styles from '../../styles/Dashboard.module.css';
+import styles from '../../styles/Config.module.css';
 import Head from 'next/head';
-import { Paper, Alert, Stack, Snackbar, Tooltip } from '@mui/material';
+import { Paper, Button, Stack, Snackbar, Tooltip, TextField } from '@mui/material';
 import axios from 'axios';
 import PlaceIcon from '@mui/icons-material/Place';
 import { useRouter } from 'next/router';
@@ -19,6 +19,11 @@ const center = {
   lng: -51.2287,
 };
 
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+};
+
 export default function Home() {
     return (
       <div>
@@ -30,7 +35,39 @@ export default function Home() {
         <link rel="icon" href="/config.png" />
       </Head>
         <main className={styles.main}>
+          <Paper className={styles.card}>
+        <div>
           <MyMap center={center}/>
+        <div>
+
+        <form onSubmit={handleSubmit}>
+          <TextField
+              id="name"
+              label="Nome"
+              variant="outlined"
+              name="Nome"
+            />
+          <TextField
+              id="email"
+              label="E-mail"
+              variant="outlined"
+              name="E-mail"
+            />            
+          <TextField
+              id="cidade"
+              label="Cidade"
+              variant="outlined"
+              name="Cidade"
+            /> 
+        <Button type="submit" variant="contained">
+                Salvar
+              </Button>                       
+        </form>
+        </div>
+
+              </div>
+          </Paper>
+
         </main>
       </div>
       </div>
