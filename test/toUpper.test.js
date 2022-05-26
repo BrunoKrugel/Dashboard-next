c17333720c7e9de037d7f56c415d26bf976e190b
Bruno Krugel <brunokrugel@gmail.com>
2022-05-26 12:59:35 -0300

    Update toUpper.test.js

--- test/toUpper.test.js
+++ test/toUpper.test.js
@@ -1,3 +1,14 @@
-test('TEST', () => {
-  expect('1').toBe('1');
+const toUpper = require('../lib/string/toUpper');
+
+
+test('Test if the first letter is upper case', () => {
+  expect(toUpper('aaaa')).toBe('Aaaa');
+});
+
+test('Test with all upper case', () => {
+  expect(toUpper('AAAA')).not.toBe('aaaa');
+});
+
+test('Test to not be lower case', () => {
+  expect(toUpper('aaaa')).not.toBe('aaaa');
 });
